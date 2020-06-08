95629c8741d0139589f5d90604cc57f92f4abeb2
malhakeem <m-rkh@hotmail.com>
2020-06-08 23:45:19 +0200

    access rights changes + language

--- article/models.py
+++ article/models.py
@@ -6,8 +6,8 @@ class Article(models.Model):
     author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name = "Yazar ")
     title = models.CharField(max_length = 50,verbose_name = "Başlık")
     content = RichTextField()
-    created_date = models.DateTimeField(auto_now_add=True,verbose_name="Oluşturulma Tarihi")
-    article_image = models.FileField(blank = True,null = True,verbose_name="Makaleye Fotoğraf Ekleyin")
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name="Created Date")
+    article_image = models.FileField(blank = True,null = True,verbose_name="Add photo to article")
     slug = models.SlugField(unique=True, max_length=100)
 
     def __str__(self):
@@ -21,9 +21,9 @@ class Article(models.Model):
     class Meta:
         ordering = ['-created_date']
 class Comment(models.Model):
-    article = models.ForeignKey(Article,on_delete = models.CASCADE,verbose_name = "Makale",related_name="comments")
-    comment_author = models.CharField(max_length = 50,verbose_name = "İsim")
-    comment_content = models.CharField(max_length = 200,verbose_name = "Yorum")
+    article = models.ForeignKey(Article,on_delete = models.CASCADE,verbose_name = "Article",related_name="comments")
+    comment_author = models.CharField(max_length = 50,verbose_name = "Name")
+    comment_content = models.CharField(max_length = 200,verbose_name = "Comment")
     comment_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.comment_content
--- article/views.py
+++ article/views.py
@@ -37,7 +37,7 @@ def addArticle(request):
         article.author = request.user
         article.save()
 
-        messages.success(request,"Makale başarıyla oluşturuldu")
+        messages.success(request,"Article Created Successfully")
         return redirect("article:dashboard")
     return render(request,"addarticle.html",{"form":form})
 def detail(request,slug):
@@ -56,7 +56,7 @@ def updateArticle(request, slug):
         article.author = request.user
         article.save()
 
-        messages.success(request,"Makale başarıyla güncellendi")
+        messages.success(request,"Article successfully updated")
         return redirect("article:dashboard")
 
 
@@ -67,7 +67,7 @@ def deleteArticle(request,slug):
 
     article.delete()
 
-    messages.success(request,"Makale Başarıyla Silindi")
+    messages.success(request,"Article Successfully Deleted")
 
     return redirect("article:dashboard")
 def addComment(request,slug):
